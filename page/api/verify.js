f248773c722ab51aefac8fd2cfb88d1b8318607c
limuvai097 <anamuljahab097@gmail.com>
2025-06-30 03:13:44 +0600

    Update verify.js

--- page/api/verify.js
+++ page/api/verify.js
@@ -1,1 +1,11 @@
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    const { code } = req.body;
 
+    console.log('Received verification code:', code);
+
+    res.status(200).send('Verification code received!');
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
+}
